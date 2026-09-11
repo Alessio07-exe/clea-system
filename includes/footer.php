c82a30d9a17c2000d0aad2be9490560299d7f698
Alessio07-exe <alessio.colombo07@gmail.com>
2026-09-11 13:54:20 +0200

    Add footer template

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * CLEA System - Footer
+ */
+?>
+
+    </div><!-- End container -->
+
+    <footer class="footer">
+        <p>&copy; 2026 <?php echo APP_NAME; ?> - Sistema di Gestione Manutenzione Macchinari</p>
+    </footer>
+
+    <script src="<?php echo APP_URL; ?>/assets/js/script.js"></script>
+</body>
+</html>
